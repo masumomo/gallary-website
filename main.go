1a879498d8d6d8d3555a7613552008bae4a3fb2f
Miki Masumoto <masumotomiki18@gmail.com>
2020-02-24 22:08:05 +0900

    Modigy port

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	_ "firstBee/routers"
 	"github.com/astaxie/beego"
+	"os"
+	"strconv"
 )
 
 func main() {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		beego.BConfig.Listen.HTTPPort = port
+	}
 	beego.Run()
 }
 
